6c0b7bd8de9726f5e5f1451326adad2bcd9b0697
sungu-che <sungu.che@icloud.com>
2017-03-10 17:48:38 +0900

    11
    11

---
+++ lang/ko.js
@@ -0,0 +1,24 @@
+(function(bom, dom) {
+	window.forum.init.lang({
+		title : {
+			reply : "답장 내용을 입력해주세요"	
+		},
+		status : {
+			none : "더이상 데이터가 없습니다."
+		},
+		placeholder : {
+			search : "검색어를 입력해주세요"
+		},
+		complete : {
+			delete : "삭제가 완료되었습니다"
+		},
+		validation : {
+			content : "내용을 입력해주세요",
+			delete : "정말로 삭제하시겠어요?",
+			keyword : "키워드를 입력해주세요",
+			login : "로그인을 해주세요"
+		}
+	})
+
+	dom.querySelector(".placeholder").innerHTML = "검색어를 입력해주세요";
+})(window, document);
